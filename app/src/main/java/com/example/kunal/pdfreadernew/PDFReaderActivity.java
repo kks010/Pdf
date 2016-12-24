1b876da2439e0cdb207d06566a352829058f9360
kunal shori <kunaladfriend@gmail.com>
2016-12-24 15:10:25 +0530

    ocr implemented

--- app/src/main/java/com/example/kunal/pdfreadernew/PDFReaderActivity.java
+++ app/src/main/java/com/example/kunal/pdfreadernew/PDFReaderActivity.java
@@ -2,21 +2,42 @@ package com.example.kunal.pdfreadernew;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.pdf.PdfRenderer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.ParcelFileDescriptor;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.widget.Toast.*;
 
 /**
  * Created by Kunal on 17-12-2016.
@@ -30,16 +51,69 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
     private static final int SWIPE_THRESHOLD_VELOCITY = 200;
     GestureDetector detector;
 
+    Bitmap bitmap;
+
+    public static final String lang = "eng";
+    public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/DemoOCR/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_reader);
 
+        imageView=(ImageView)findViewById(R.id.image);
+
         detector =  new GestureDetector(this,this);
 
 
+        //checking for language and drecting language path.
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v("Main", "ERROR: Creation of directory " + path + " on sdcard failed");
+                    break;
+                } else {
+                    Log.v("Main", "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+
+                InputStream in = assetManager.open(lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                // Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                // Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+
+
+        }
+
     }
 
+
+    //Point at which rendering takes place
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -47,6 +121,7 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
         render();               //for pdf rendering
 
     }
+
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -69,6 +144,7 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
         return false;
+
     }
 
     @Override
@@ -79,6 +155,31 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
     @Override
     public void onLongPress(MotionEvent e) {
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(PDFReaderActivity.this);
+        builder.setMessage("Show in Text");
+        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent i = new Intent(PDFReaderActivity.this,TextActivity.class);
+                i.putExtra("image",byteArray);
+                startActivity(i);
+            }
+        });
+
+        builder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //do nothing
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     @Override
@@ -103,52 +204,52 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    void render(){
-        try{
-
-            imageView=(ImageView)findViewById(R.id.image);
-            String path=null;
+    void render() {
+        try {
+//            final Animation zoomAnimation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+            String path = null;
 
             //UNPACK OUR DATA FROM INTENT
-            Intent i=this.getIntent();
+            Intent i = this.getIntent();
 
-            if(i.getAction()!= null){
-                path=i.getData().getPath();
-            }
-            else {
+            if (i.getAction() != null) {
+                path = i.getData().getPath();
+            } else {
                 path = i.getExtras().getString("PATH");
 
             }
             int REQ_WIDTH = imageView.getWidth();
             int REQ_HEIGHT = imageView.getHeight();
 
-            Bitmap bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_8888);
+            bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_8888);
 
             //GET THE PDF FILE
-            File file=new File(path);
-            PdfRenderer renderer = new PdfRenderer(ParcelFileDescriptor.open(file,ParcelFileDescriptor.MODE_READ_ONLY));
+            File file = new File(path);
+            PdfRenderer renderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
 
 
-            if(CurrentPage<0){
-                CurrentPage=0;
-            }else if(CurrentPage >renderer.getPageCount()){
-                CurrentPage=renderer.getPageCount() - 1;
+            if (CurrentPage < 0) {
+                CurrentPage = 0;
+            }else if (CurrentPage > renderer.getPageCount()) {
+                CurrentPage = renderer.getPageCount() - 1;
             }
 
+
+
             Matrix m = imageView.getImageMatrix();
-            Rect rect= new Rect(0,0,REQ_WIDTH,REQ_HEIGHT);
-            renderer.openPage(CurrentPage).render(bitmap, rect, m, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            Rect rect = new Rect(0, 0, REQ_WIDTH, REQ_HEIGHT);
+            renderer.openPage(CurrentPage).render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
             imageView.setImageMatrix(m);
             imageView.setImageBitmap(bitmap);
             imageView.invalidate();
+
+            //imageView.startAnimation(zoomAnimation);
+
             imageView.setOnTouchListener(this);
 
-
-
-        }catch(Exception e){
+        }catch (Exception e) {
             e.printStackTrace();
             Log.e("kunal", " " + e);
         }
     }
-
 }
---
+++ app/src/main/java/com/example/kunal/pdfreadernew/TextActivity.java
@@ -0,0 +1,106 @@
+package com.example.kunal.pdfreadernew;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.EditText;
+
+/**
+ * Created by Kunal on 24-12-2016.
+ */
+public class TextActivity extends Activity {
+
+    private TessOCR mTessOCR;
+    EditText editText;
+    private ProgressDialog mProgressDialog;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+
+        editText=(EditText)findViewById(R.id.set_text_editText);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+        mTessOCR = new TessOCR();
+
+
+        doOCR(convertColorIntoBlackAndWhiteImage(bitmap));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+    }
+
+    public void doOCR(final Bitmap bitmap) {
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog.show(this, "Processing",
+                    "Please wait...", true);
+            // mResult.setVisibility(V.ViewISIBLE);
+
+
+        }
+        else {
+            mProgressDialog.show();
+        }
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                final String result = mTessOCR.getOCRResult(bitmap).toLowerCase();
+
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        if (result != null && !result.equals("")) {
+                            String s = result.trim();
+                            editText.setText(result);
+
+                        }
+
+                        mProgressDialog.dismiss();
+                    }
+
+                });
+
+            };
+        }).start();
+
+
+    }
+
+
+    private Bitmap convertColorIntoBlackAndWhiteImage(Bitmap orginalBitmap) {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+
+        ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(
+                colorMatrix);
+
+        Bitmap blackAndWhiteBitmap = orginalBitmap.copy(
+                Bitmap.Config.ARGB_8888, true);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(colorMatrixFilter);
+
+        Canvas canvas = new Canvas(blackAndWhiteBitmap);
+        canvas.drawBitmap(blackAndWhiteBitmap, 0, 0, paint);
+
+        return blackAndWhiteBitmap;
+    }
+}
