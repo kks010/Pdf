66a4e60a19486590c41011d0a3de56946115f927
kunal shori <kunaladfriend@gmail.com>
2016-12-24 17:10:13 +0530

    text file conversion

--- app/src/main/java/com/example/kunal/pdfreadernew/PDFReaderActivity.java
+++ app/src/main/java/com/example/kunal/pdfreadernew/PDFReaderActivity.java
@@ -1,10 +1,17 @@
 package com.example.kunal.pdfreadernew;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -20,6 +27,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -27,6 +36,7 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -36,6 +46,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import static android.widget.Toast.*;
 
@@ -53,8 +64,17 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
 
     Bitmap bitmap;
 
+    private TessOCR mTessOCR;
+    private ProgressDialog mProgressDialog;
+
     public static final String lang = "eng";
     public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/DemoOCR/";
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +130,8 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
 
         }
 
+        mTessOCR = new TessOCR();
+
     }
 
 
@@ -154,32 +176,34 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
 
     @Override
     public void onLongPress(MotionEvent e) {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(PDFReaderActivity.this);
+//        builder.setMessage("Show in Text");
+//        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//
+//                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//                byte[] byteArray = stream.toByteArray();
+//
+//                Intent i = new Intent(PDFReaderActivity.this,TextActivity.class);
+//                i.putExtra("image",byteArray);
+//                startActivity(i);
+//            }
+//        });
+//
+//        builder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                //do nothing
+//            }
+//        });
+//
+//        AlertDialog alert = builder.create();
+//        alert.show();
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(PDFReaderActivity.this);
-        builder.setMessage("Show in Text");
-        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                byte[] byteArray = stream.toByteArray();
-
-                Intent i = new Intent(PDFReaderActivity.this,TextActivity.class);
-                i.putExtra("image",byteArray);
-                startActivity(i);
-            }
-        });
-
-        builder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //do nothing
-            }
-        });
-
-        AlertDialog alert = builder.create();
-        alert.show();
+        doOCR(convertColorIntoBlackAndWhiteImage(bitmap));
     }
 
     @Override
@@ -247,9 +271,135 @@ public class PDFReaderActivity extends Activity implements View.OnTouchListener,
 
             imageView.setOnTouchListener(this);
 
+
         }catch (Exception e) {
             e.printStackTrace();
             Log.e("kunal", " " + e);
         }
     }
+
+    public void doOCR(final Bitmap bitmap) {
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog.show(this, "Processing",
+                    "Please wait...", true);
+            // mResult.setVisibility(V.ViewISIBLE);
+
+
+        }
+        else {
+            mProgressDialog.show();
+        }
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                final String result = mTessOCR.getOCRResult(bitmap).toLowerCase();
+
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        if (result != null && !result.equals("")) {
+                            String s = result.trim();
+                            //save as text
+                            writeToFile(result);
+
+
+                        }
+
+                        mProgressDialog.dismiss();
+                    }
+
+                });
+
+            };
+        }).start();
+
+
+    }
+
+
+    private Bitmap convertColorIntoBlackAndWhiteImage(Bitmap orginalBitmap) {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+
+        ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(
+                colorMatrix);
+
+        Bitmap blackAndWhiteBitmap = orginalBitmap.copy(
+                Bitmap.Config.ARGB_8888, true);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(colorMatrixFilter);
+
+        Canvas canvas = new Canvas(blackAndWhiteBitmap);
+        canvas.drawBitmap(blackAndWhiteBitmap, 0, 0, paint);
+
+        return blackAndWhiteBitmap;
+    }
+
+    public void writeToFile(String data) {
+        // Get the directory for the user's public pictures directory.
+        verifyStoragePermissions(PDFReaderActivity.this);
+        File path =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        File file = new File(path, "ToText.txt");
+
+        // Save your stream, don't forget to flush() it before closing it.
+
+        try
+        {
+
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+
+            myOutWriter.close();
+
+            fOut.flush();
+            fOut.close();
+
+            NotificationCompat.Builder builder= new NotificationCompat.Builder(getApplicationContext());
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            builder.setContentTitle("Text File Saved");
+            builder.setContentText(path.toString());
+
+            Notification notification = builder.build();
+            NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            notification.defaults |= Notification.DEFAULT_VIBRATE;
+            notification.defaults |= Notification.DEFAULT_SOUND;
+
+            nm.notify(1, notification);
+        }
+        catch (Exception e)
+        {
+            Log.e("kunal", "File write failed: " + e.toString());
+        }
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
 }
